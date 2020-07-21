720984668ed628c6029163995c69f5ab0e77361c
Gareth Harris <gdharris1000@gmail.com>
2020-07-21 15:04:49 +0100

    set up living and checkbox components

--- App.js
+++ App.js
@@ -1,114 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+// import Nav from './components/nav';
+// import Self from './components/self';
+// import Play from './components/play';
+import Living from './components/living';
+// import Work from './components/work';
+// import Total from './components/total';
+import {StyleSheet, ScrollView, SafeAreaView} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default function App() {
+  const [selfCount, setSelfCount] = useState(0);
+  const [playCount, setPlayCount] = useState(0);
+  const [livingCount, setLivingCount] = useState(0);
+  const [workCount, setWorkCount] = useState(0);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const incrementSelfCount = (isSelected) => {
+    isSelected ? setSelfCount(selfCount - 1) : setSelfCount(selfCount + 1);
+  };
 
-const App: () => React$Node = () => {
+  const incrementPlayCount = (isSelected) => {
+    isSelected ? setPlayCount(playCount - 1) : setPlayCount(playCount + 1);
+  };
+
+  const incrementLivingCount = (isSelected) => {
+    isSelected
+      ? setLivingCount(livingCount - 1)
+      : setLivingCount(livingCount + 1);
+  };
+
+  const incrementWorkCount = (isSelected) => {
+    isSelected ? setWorkCount(workCount - 1) : setWorkCount(workCount + 1);
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {/* <Nav /> */}
+        {/* <Self incrementSelfCount={incrementSelfCount} selfCount={selfCount} /> */}
+        {/* <Play incrementPlayCount={incrementPlayCount} playCount={playCount} /> */}
+        <Living
+          incrementLivingCount={incrementLivingCount}
+          livingCount={livingCount}
+        />
+        {/* <Work incrementWorkCount={incrementWorkCount} workCount={workCount} /> */}
+        {/* <Total
+          livingCount={livingCount}
+          workCount={workCount}
+          selfCount={selfCount}
+          playCount={playCount}
+        /> */}
+      </ScrollView>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+    backgroundColor: 'white',
   },
 });
-
-export default App;
