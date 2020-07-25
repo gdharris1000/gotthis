90b72e72133b3cbd4e8b7ae86a8c319a9f3c27ba
Gareth Harris <gdharris1000@gmail.com>
2020-07-25 15:24:12 +0100

    add achievments with categories

--- components/add-achievment.js
+++ components/add-achievment.js
@@ -6,46 +6,85 @@ import {
   View,
   TouchableOpacity,
 } from 'react-native';
-import {RadioButton} from 'react-native-paper';
+// import {CheckBox} from 'react-native-elements';
+import CheckBox from '@react-native-community/checkbox';
 
 export default function AddAchievement(props) {
   const [achievement, setAchievement] = useState('');
-  const [category, setCategory] = useState('Self');
+  const [category, setCategory] = useState([]);
+  const [isSelected, setSelection] = useState([
+    {category: 'Work', selected: false},
+    {category: 'Self', selected: false},
+    {category: 'Living', selected: false},
+    {category: 'Play', selected: false},
+  ]);
 
-  const onChangeAchievement = (text) => {
-    setAchievement(text);
+  const updateValue = (index) => {
+    let newArr = [...isSelected];
+    newArr[index].selected = !isSelected[index].selected;
+    setSelection(newArr);
+    var cat = [];
+    isSelected.map((item) => {
+      if (item.selected === true) {
+        cat.push(item.category);
+
+        // setCategory((prevItems) => {
+        //   console.log('item: ' + item.category);
+        //   return [item.category, ...prevItems];
+        // });
+      }
+    });
+    setCategory(cat);
   };
+
+  const submitAchievement = () => {
+    console.log('categoryies:' + category);
+    props.addAchievement(achievement, category);
+  };
+
 
   return (
     <View style={styles.container}>
       <TextInput
         placeholder="Enter achievement"
-        onChangeText={onChangeAchievement}
+        onChangeText={setAchievement}
         style={styles.txtInput}
         multiline
         numberOfLines={6}
       />
+      <Text>Where does it sit?</Text>
 
-      <View style={styles.btnView}>
+      {isSelected.map((item, index) => (
+        <>
+          <Text>{item.category}</Text>
+          <CheckBox
+            disabled={false}
+            value={item.selected}
+            onValueChange={() => updateValue(index)}
+          />
+        </>
+      ))}
+
+      {/* <View style={styles.btnView}>
         <RadioButton
           value="Self"
           status={category === 'Self' ? 'checked' : 'unchecked'}
           onPress={() => setCategory('Self')}
         />
         <Text style={styles.radioBtnText}>Self</Text>
-      </View>
-
-      <View style={styles.btnView}>
-        <RadioButton
-          value="Work"
-          status={category === 'Work' ? 'checked' : 'unchecked'}
-          onPress={() => setCategory('Work')}
+      </View>gory('Work')}
         />
         <Text style={styles.radioBtnText}>Work</Text>
       </View>
 
       <View style={styles.btnView}>
         <RadioButton
+
+      <View style={styles.btnView}>
+        <RadioButton
+          value="Work"
+          status={category === 'Work' ? 'checked' : 'unchecked'}
+          onPress={() => setCate
           value="Living"
           status={category === 'Living' ? 'checked' : 'unchecked'}
           onPress={() => setCategory('Living')}
@@ -60,11 +99,8 @@ export default function AddAchievement(props) {
           onPress={() => setCategory('Play')}
         />
         <Text style={styles.radioBtnText}>Play</Text>
-      </View>
-
-      <TouchableOpacity
-        style={styles.btn}
-        onPress={() => props.addAchievement(achievement, category)}>
+      </View> */}
+      <TouchableOpacity style={styles.btn} onPress={() => submitAchievement()}>
         <Text style={styles.btnText}>Add Achievement</Text>
       </TouchableOpacity>
     </View>
@@ -100,5 +136,8 @@ const styles = StyleSheet.create({
     fontSize: 18,
     padding: 10,
     marginTop: 10,
+  },
+  checkbox: {
+    alignSelf: 'center',
   },
 });
